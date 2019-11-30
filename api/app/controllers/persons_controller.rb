71e9fc591ffb8fad02235bd31b332200f21c6f8c
Yasyf Mohamedali <yasyf@meetkaruna.com>
2019-11-30 04:31:51 -0800

    Allow updating RSVP via API.

--- api/app/controllers/persons_controller.rb
+++ api/app/controllers/persons_controller.rb
@@ -5,11 +5,33 @@ class PersonsController < ApplicationController
     render json: Person.all
   end
 
+  def update
+    if update_params[:committed] == true
+      person.reservation.update! status: :committed
+    elsif update_params[:committed] == false
+      person.reservation.update! status: :rejected
+    end
+
+    if update_params[:flight_number].present?
+      person.reservation.update! flight_number: update_params[:flight_number]
+    end
+
+    render json: person
+  end
+
   def find
     render json: Person.where(**find_params.to_h.symbolize_keys).first
   end
 
   private
+
+  def person
+    @person ||= Person.find(params[:id])
+  end
+
+  def update_params
+    params.require(:person).permit(:committed, :flight_number)
+  end
 
   def find_params
     params.require(:person).permit(:email_address)
--- api/app/models/reservation.rb
+++ api/app/models/reservation.rb
@@ -3,5 +3,5 @@
 class Reservation < ApplicationRecord
   belongs_to :person
 
-  enum status: { pending: 0, comitted: 1, confirmed: 2, rejected: 3 }
+  enum status: { pending: 0, committed: 1, confirmed: 2, rejected: 3 }, _suffix: true
 end
