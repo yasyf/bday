544a29d293e0984f414a5137ae55f43e489182c9
Yasyf Mohamedali <yasyf@meetkaruna.com>
2019-12-01 03:26:51 -0800

    Fix default host.

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -104,6 +104,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  config.default_url_options = config.action_controller.default_url_options = config.action_mailer.default_url_options = { host: "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" }
 end
+
+Rails.application.routes.default_url_options[:host] = "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
